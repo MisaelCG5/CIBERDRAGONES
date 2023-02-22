e26f00087f045601db1002cf991cb469c253a71e
unknown <18307014@utfv.edu.mx>
2023-02-22 17:22:00 -0600

    22/02/2023

---
+++ Larespuesta.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+header("content-Type: text/html; charset=UTF-8");
+
+$usuario = isset($_POST['usuario']) ?  $_POST['usuario']: '' ;
+$password = isset($_POST['password']) ?  $_POST['password']: '' ;
+$usuarioTabla = '';
+$passwordTabla = '';
+
+$con = new SQLite3("data.db");
+$cs = $con ->  query (" SELECT * FROM login WHERE usuario = '$usuario' AND password = '$password' ");
+while ( $result = $cs -> fetchArray()) {
+
+    $usuarioTabla = $result['usuario'];
+    $passwordTabla = $result['password'];
+   
+}
+
+if ($usuario == $usuarioTabla){
+    if ($password == $passwordTabla){
+echo 'USUARIO CORRECTO';
+echo "<br>";
+    }
+
+} else {
+   
+    echo 'USUARIO INCORRECTO';
+     echo "<br>";
+}
+
+
+
+//$password = $_POST['password']; sin el ISSET
+//$ = isset($_POST[' ']) ? $_POST[' '] : '' ;
+?>
